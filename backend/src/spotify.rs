e079fc347b393a265b0e9a8ea27d232049a5cd67
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-08-05 21:17:29 +0200

    Spotify loggin

--- backend/src/spotify.rs
+++ backend/src/spotify.rs
@@ -1,13 +1,31 @@
+use std::env;
+
 use serde::Serialize;
 
 #[derive(Serialize, Debug)]
 pub struct SpotifyLogin {
-    pub username: String,
-    pub password: String,
+    pub client_id: String,
+    pub client_secret: String,
 }
 
 impl SpotifyLogin {
-    pub fn new(username: String, password: String) -> SpotifyLogin {
-        SpotifyLogin { username, password }
+    pub fn new(client_id: String, client_secret: String) -> SpotifyLogin {
+        SpotifyLogin {
+            client_id,
+            client_secret,
+        }
+    }
+}
+
+impl Default for SpotifyLogin {
+    fn default() -> Self {
+        let client_id = env::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID not set");
+        let client_secret =
+            env::var("SPOTIFY_CLIENT_SECRET").expect("SPOTIFY_CLIENT_SECRET not set");
+
+        SpotifyLogin {
+            client_id,
+            client_secret,
+        }
     }
 }
