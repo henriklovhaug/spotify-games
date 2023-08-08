97071c05c0a2de89e73ca8e2a55a0afa91056737
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-08-08 11:14:20 +0200

    add try fetch refresh token from file

--- backend/src/spotify/token.rs
+++ backend/src/spotify/token.rs
@@ -2,6 +2,7 @@ use std::{env, error::Error};
 
 use base64::{engine::general_purpose, Engine};
 use reqwest::Client;
+use tokio::{fs::File, io::AsyncReadExt};
 
 use crate::{
     spotify::response_types::LoginResponse,
@@ -24,8 +25,7 @@ pub async fn login(store: Store, code: String) -> Result<(), Box<dyn Error>> {
     let client_id = env::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID not set");
     let client_secret = env::var("SPOTIFY_CLIENT_SECRET").expect("SPOTIFY_CLIENT_SECRET not set");
 
-    let client_id_secret = format!("{}:{}", client_id, client_secret);
-    let base64_auth = general_purpose::STANDARD.encode(client_id_secret.as_bytes());
+    let base64_auth = base64_encode(&client_id, &client_secret);
 
     let response = client
         .post(TOKEN_URL)
@@ -47,4 +47,60 @@ pub async fn login(store: Store, code: String) -> Result<(), Box<dyn Error>> {
     store.set_token(token).await;
 
     Ok(())
+}
+
+pub async fn refresh_token(store: Store, refresh_token: &str) -> Result<(), Box<dyn Error>> {
+    let client = CLIENT.get_or_init(Client::new);
+
+    let params = [
+        ("grant_type", "refresh_token"),
+        ("refresh_token", refresh_token),
+    ];
+
+    let client_id = env::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID not set");
+    let client_secret = env::var("SPOTIFY_CLIENT_SECRET").expect("SPOTIFY_CLIENT_SECRET not set");
+
+    let base64_auth = base64_encode(&client_id, &client_secret);
+
+    let response = client
+        .post(TOKEN_URL)
+        .header("Authorization", format!("Basic {}", base64_auth))
+        .form(&params)
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        println!("Refresh token successfull");
+    } else {
+        println!("Refresh token failed");
+        println!("{:?}", response);
+        return Err("Refresh token failed".into());
+    }
+
+    let token: Token = response.json::<LoginResponse>().await?.into();
+
+    store.set_token(token).await;
+
+    Ok(())
+}
+
+fn base64_encode(id: &str, secret: &str) -> String {
+    let id_secret = format!("{}:{}", id, secret);
+    let base64_auth = general_purpose::STANDARD.encode(id_secret.as_bytes());
+    base64_auth
+}
+
+pub async fn restore_token_from_file(store: Store) -> Result<(), Box<dyn Error>> {
+    let path = dirs::data_dir()
+        .ok_or::<String>("File not found".into())?
+        .join("spotify-games")
+        .join("data.txt");
+
+    let mut file = File::open(path).await?;
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).await?;
+
+    Ok(refresh_token(store, &contents).await?)
 }
