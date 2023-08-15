ebbc5fbbfffd080c551ab9a7e981265c4f05f369
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-08-15 17:32:55 +0200

    Improved search

--- client/src/lib/components/search.svelte
+++ client/src/lib/components/search.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { afterUpdate } from 'svelte';
 
 	function handle_submit(event: Event) {
 		event.preventDefault();
@@ -10,26 +11,35 @@
 		visible = !visible;
 	}
 
+	afterUpdate(() => {
+		if (visible) {
+			input.focus();
+		}
+	});
+
 	let visible = false;
 	let search = '';
+
+	let input: HTMLInputElement;
 </script>
 
 {#if !visible}
-	<div class="h-full">
-		<button class="h-fit border-none bg-none" on:click={() => (visible = !visible)}>
-			<img src="icons/search-icon.png" alt="search icon" class=" h-full pr-2 pt-2 object-contain" />
+	<div class="h-full object-contain">
+		<button class="h-full border-none bg-none" on:click={() => (visible = !visible)}>
+			<img src="icons/search-icon.png" alt="search icon" class=" h-5/6 pr-2 pt-2" />
 		</button>
 	</div>
 {:else}
 	<form class="m-0 flex w-screen justify-center pt-2" method="post" action="?/search" use:enhance>
 		<label for="search-box">
 			<input
-				class="h-72 w-screen rounded-md"
+				class="h-6 w-4/5 rounded-md bg-neutral-200"
 				name="search"
 				type="text"
 				id="search-box"
 				on:focusout={handle_submit}
 				value={search}
+				bind:this={input}
 			/>
 		</label>
 	</form>
---
+++ client/src/routes/game/[slug]/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="flex w-full flex-col">
+  <p>TODO</p>
+</div>
