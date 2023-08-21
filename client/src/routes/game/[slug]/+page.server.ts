6fde9ba7aae917962a95443e59000a6fa70f9cab
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-08-21 19:42:18 +0200

    Update format options

--- client/src/routes/game/[slug]/+page.server.ts
+++ client/src/routes/game/[slug]/+page.server.ts
@@ -3,18 +3,18 @@ import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
-  let game: string = params.slug;
+	let game: string = params.slug;
 
-  const response = await fetch(`http://${BACKEND_URL}/game/${game}`, {
-    method: "PUT",
-  });
+	const response = await fetch(`http://${BACKEND_URL}/game/${game}`, {
+		method: "PUT",
+	});
 
-  if (response.ok) {
-    console.log("Game successfully updated");
-    return {
-      success: true,
-    };
-  } else {
-    throw redirect(303, "/");
-  }
+	if (response.ok) {
+		console.log("Game successfully updated");
+		return {
+			success: true,
+		};
+	} else {
+		throw redirect(303, "/");
+	}
 };
--- client/src/routes/search/[slug]/+page.server.ts
+++ client/src/routes/search/[slug]/+page.server.ts
@@ -4,45 +4,43 @@ import { type Actions, fail } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
-  const slug = params.slug;
+	const slug = params.slug;
 
-  const response = await fetch(
-    `http://${BACKEND_URL}/search?search=${slug}`,
-  );
-  if (response.ok) {
-    const json: Song[] = await response.json();
-    return {
-      body: json,
-    };
-  } else {
-    return fail(400, { message: "Bad Request" });
-  }
+	const response = await fetch(`http://${BACKEND_URL}/search?search=${slug}`);
+	if (response.ok) {
+		const json: Song[] = await response.json();
+		return {
+			body: json,
+		};
+	} else {
+		return fail(400, { message: "Bad Request" });
+	}
 };
 
 export const actions: Actions = {
-  queue: async ({ request, fetch }) => {
-    const data = await request.formData();
+	queue: async ({ request, fetch }) => {
+		const data = await request.formData();
 
-    const song: Song = {
-      id: data.get("id") as string,
-      name: data.get("name") as string,
-      artist: data.get("artist") as string,
-      album: data.get("album") as string,
-      duration: +!data.get("duration"),
-    };
+		const song: Song = {
+			id: data.get("id") as string,
+			name: data.get("name") as string,
+			artist: data.get("artist") as string,
+			album: data.get("album") as string,
+			duration: +!data.get("duration"),
+		};
 
-    console.log(JSON.stringify(song) + "stonk");
+		console.log(JSON.stringify(song) + "stonk");
 
-    const response = await fetch(`http://${BACKEND_URL}/queue`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(song),
-    });
+		const response = await fetch(`http://${BACKEND_URL}/queue`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(song),
+		});
 
-    if (!response.ok) {
-      return fail(400, { message: "Bad Request" });
-    }
-  },
+		if (!response.ok) {
+			return fail(400, { message: "Bad Request" });
+		}
+	},
 };
