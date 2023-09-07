0ca0d9256d9e8b5c156c4ad075abe5b35ba46fd1
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2023-09-07 21:59:23 +0200

    Added queue popup

--- backend/src/routes/queue.rs
+++ backend/src/routes/queue.rs
@@ -1,12 +1,25 @@
 use axum::{extract::State, http::StatusCode, Json};
 
-use crate::{spotify::types::Song, store::Store};
+use crate::{
+    spotify::types::{QueueSong, Song},
+    store::Store,
+    Channel, ChannelMessage,
+};
 
 pub async fn add_to_queue_handler(
     State(store): State<Store>,
     Json(track): Json<Song>,
 ) -> StatusCode {
-    store.add_song_to_queue(track).await;
+    store.add_song_to_queue(track.clone()).await;
+    let sender = store.get_sender();
+    let queue_song: QueueSong = track.into();
+    let message = ChannelMessage::new(
+        Channel::QueueSong,
+        "".into(),
+        Some(queue_song.get_artist().into()),
+        Some(queue_song.get_name().into()),
+    );
+    let _ = sender.send(message);
     StatusCode::CREATED
 }
 
---
+++ client/src/lib/components/queuePopup.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { QueueSong } from "$lib/types";
+  import { fly } from "svelte/transition";
+
+  let className: string;
+
+  export { className as class };
+
+  export let active: boolean = false;
+  export let song: QueueSong;
+
+  // Uncomment this to test the popup
+  $: if (active) {
+    setTimeout(() => {
+      active = false;
+    }, 3000);
+  }
+</script>
+
+{#if active}
+  <div
+    transition:fly={{ delay: 250, duration: 700 }}
+    class="flex w-full flex-col items-center bg-green {className}"
+  >
+    <h1 class="my-2 text-3xl text-black">{song.name} - {song.artist}</h1>
+    <h2 class="my-2 text-black">Was added to the queue</h2>
+  </div>
+{/if}
