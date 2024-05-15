5d70b4ccefb525738e40f9727f5ec6e91e64a69d
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2024-05-15 15:07:50 +0200

    Add missing games

--- backend/src/routes/index.rs
+++ backend/src/routes/index.rs
@@ -7,27 +7,45 @@ pub async fn index_handler() -> IndexTemplate<'static> {
 #[derive(Template)]
 #[template(path = "index.html")]
 pub struct IndexTemplate<'a> {
-    games: &'a [GamePlayStruct<'a>],
+    games: &'a [GamePlayModal<'a>],
 }
 
-pub struct GamePlayStruct<'a> {
+pub struct GamePlayModal<'a> {
     pub name: &'a str,
     pub id: &'a str,
     pub url: &'a str,
     pub target: &'a str,
 }
 
-static GAMES: [GamePlayStruct; 2] = [
-    GamePlayStruct {
+static GAMES: [GamePlayModal; 5] = [
+    GamePlayModal {
         name: "Opus",
         id: "opus",
         url: "/Opus",
         target: "opus-t",
     },
-    GamePlayStruct {
+    GamePlayModal {
         name: "Six Minutes",
         id: "six",
         url: "/SixMinutes",
         target: "six-t",
+    },
+    GamePlayModal {
+        name: "Rattling Bog",
+        id: "rattling",
+        url: "/RattlingBog",
+        target: "rattling-t",
+    },
+    GamePlayModal {
+        name: "Thunder",
+        id: "thunder",
+        url: "/Thunder",
+        target: "thunder-t",
+    },
+    GamePlayModal {
+        name: "Palmerna",
+        id: "palmerna",
+        url: "/Palmerna",
+        target: "palmerna-t",
     },
 ];
