2a16a7db5e51f5d0401e650d352c8f530d59ec38
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2024-05-15 17:10:49 +0200

    Delete client

--- client/src/lib/gamemodes.ts
+++
@@ -1,4 +0,0 @@
-export interface GameMode {
-  name: string;
-  url: string;
-}
--- client/src/routes/callback/+server.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from "@sveltejs/kit";
-
-export const POST: RequestHandler = async ({ request }) => {
-  console.log("callback server", request.body);
-
-  return new Response(JSON.stringify({ message: "callback server" }));
-};
